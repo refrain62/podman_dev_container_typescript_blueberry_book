90858d7fc8dbd90146c34ce129ae7bc78e6fbc39
refrain62 <nao_h13@hotmail.com>
2024-06-02 07:28:52 +0000

    8syou

--- practice/src/7/7.2.1.ts
+++ practice/src/7/7.2.1.ts
@@ -8,5 +8,5 @@ const rl = createInterface({
 rl.question('文字列を入力してください:', (line) => {
   // 文字列が入力されるとここが実行される
   console.log(`${line}が入力されました`)
-  rl.console()
+  rl.close()
 })
---
+++ practice/src/8/8.5.2.ts
@@ -0,0 +1,9 @@
+import path from "path";
+import { readFile } from "fs/promises";
+import { fileURLToPath } from "url";
+
+const filePath = fileURLToPath(import.meta.url);
+const fileDir = path.dirname(filePath);
+const dataFile = path.join(fileDir, "../uhyo.txt");
+
+const data = await readFile(dataFile, { encoding: "utf8" });
